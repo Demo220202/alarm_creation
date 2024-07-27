403288dd46a6fd2acb0dadbd652ce5950608f8d1
Demo220202 <158024374+Demo220202@users.noreply.github.com>
2024-07-28 02:03:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,52 @@
-provider "aws" {
-  region = var.region
+data "aws_sns_topic" "selected" {
+  name = var.sns_name
+}
+
+# Fetch the instance ID using the instance name (for EC2)
+data "aws_instance" "selected" {
+  count = var.service_type == "EC2" ? 1 : 0
+
+  filter {
+    name   = "tag:Name"
+    values = [var.instance_name]
+  }
+  most_recent = true
+}
+
+# Fetch the DB instance ID using the instance name (for RDS)
+data "aws_db_instance" "selected" {
+  count = var.service_type == "RDS" ? 1 : 0
+
+  filter {
+    name   = "db-instance-id"
+    values = [var.instance_name]
+  }
+}
+
+# Determine dimensions based on service type
+locals {
+  dimensions = var.service_type == "EC2" ? { InstanceId = data.aws_instance.selected[0].id } :
+               var.service_type == "RDS" ? { DBInstanceIdentifier = data.aws_db_instance.selected[0].id } :
+               {}
 }
 
 resource "aws_cloudwatch_metric_alarm" "example" {
-  alarm_name          = var.alarm_name
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
-  metric_name         = var.metric_name
-  namespace           = var.namespace
-  period              = "300"
-  statistic           = "Average"
-  threshold           = var.threshold
+  alarm_name                = "${var.instance_name}-${var.metric_name}-alarm"
+  comparison_operator       = var.comparison_operator
+  evaluation_periods        = var.evaluation_periods
+  metric_name               = var.metric_name
+  namespace                 = var.namespace
+  period                    = var.period
+  statistic                 = "Average"
+  threshold                 = var.threshold
+  alarm_description         = "Alarm for ${var.instance_name} on metric ${var.metric_name}"
+  actions_enabled           = true
+  alarm_actions             = [data.aws_sns_topic.selected.arn]
+  ok_actions                = [data.aws_sns_topic.selected.arn]
+  insufficient_data_actions = [data.aws_sns_topic.selected.arn]
+  dimensions                = local.dimensions
 
-  dimensions = {
-    InstanceId = var.instance_id
+  lifecycle {
+    create_before_destroy = true
   }
-
-  alarm_description = "This metric monitors the ${var.metric_name} for instance ${var.instance_id}"
-  actions_enabled   = true
-  alarm_actions     = [var.sns_topic_arn]
-  ok_actions        = [var.sns_topic_arn]
-  insufficient_data_actions = [var.sns_topic_arn]
 }
